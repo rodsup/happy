a490e22c89fc02884aba1ba9bf9b312ea1793ee2
rodsup <rodnascimentolopes@gmail.com>
2020-11-11 01:40:18 -0300

    Updates

---
+++ precache-manifest.9f7657bb6b7a324dd33ef6c444ef4a18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7211fd3c6b06458ba8009dd589965ad1",
+    "url": "/happy/index.html"
+  },
+  {
+    "revision": "e41741884dbc692294fa",
+    "url": "/happy/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "20a5f8c239fb84cd7669",
+    "url": "/happy/static/css/main.dc6fb400.chunk.css"
+  },
+  {
+    "revision": "e41741884dbc692294fa",
+    "url": "/happy/static/js/2.4eccfe35.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/happy/static/js/2.4eccfe35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a5f8c239fb84cd7669",
+    "url": "/happy/static/js/main.6f80a22c.chunk.js"
+  },
+  {
+    "revision": "beffc59a84efbb1596c3",
+    "url": "/happy/static/js/runtime-main.61df2244.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/happy/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/happy/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/happy/static/media/map-marker.658b8302.svg"
+  }
+]);
